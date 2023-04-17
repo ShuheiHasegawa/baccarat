75504dae25154ebe82bbf8da4c437880d76fa637
Shuhei Hasegawa <shuhei.hasegawa1989@gmail.com>
2023-04-18 00:22:39 +0900

    Initial Commit

---
+++ baccarat Watch App/baccaratApp.swift
@@ -0,0 +1,17 @@
+//
+//  baccaratApp.swift
+//  baccarat Watch App
+//
+//  Created by Shuhei Hasegawa on 2023/04/18.
+//
+
+import SwiftUI
+
+@main
+struct baccarat_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
