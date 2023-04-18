3d441841546e5f790e4136d4619db59addbc801d
Shuhei Hasegawa <shuhei.hasegawa1989@gmail.com>
2023-04-18 20:56:31 +0900

    スワイプジェスチャーと振動通知イベントの作成

--- baccarat Watch App/ContentView.swift
+++ baccarat Watch App/ContentView.swift
@@ -8,14 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
+//    var body: some View {
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+//            Text("Hello, world!")
+//        }
+//        .padding()
+//    }
+    
+    
+//    @State var flg = true
+//
+//    var body: some View {
+//        VStack {
+//            if self.flg {
+//                Text("Hello World!")
+//                    .onTapGesture {
+//                        self.flg.toggle()
+//                }
+//            }
+//            else{
+//                Text("Tapped!")
+//                    .onTapGesture {
+//                        self.flg.toggle()
+//                }
+//            }
+//        }
+//    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Baccarat")
+                // スワイプジェスチャーイベント
+                .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .local)
+                        .onEnded({ value in
+                            if value.translation.width < 0 {
+                                // left
+                                print("left!")
+                            }
+
+                            if value.translation.width > 0 {
+                                // right
+                                print("right!")
+                            }
+                            if value.translation.height < 0 {
+                                // up
+                                print("up!")
+                            }
+
+                            if value.translation.height > 0 {
+                                // down
+                                print("down!")
+//                                knock(type: WKHapticType(rawValue: 6))
+                                WKInterfaceDevice.current().play(.success)
+                            }
+                        }))
         }
-        .padding()
+    }
+    
+    // 振動通知イベント
+    func knock(type: WKHapticType?) {
+        guard let hType = type else { return }
+        WKInterfaceDevice.current().play(hType)
     }
 }
 
---
+++ baccarat Watch App/UIView.swift
@@ -0,0 +1,20 @@
+//
+//  UIView.swift
+//  baccarat Watch App
+//
+//  Created by Shuhei Hasegawa on 2023/04/18.
+//
+
+import SwiftUI
+
+struct UIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UIView_Previews: PreviewProvider {
+    static var previews: some View {
+        UIView()
+    }
+}
